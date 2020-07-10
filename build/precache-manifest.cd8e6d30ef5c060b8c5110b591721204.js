37e70a5f805c67ca6f6c8695a085019f1cc83e43
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-07-09 22:49:02 -0500

    added build from react static app

---
+++ build/precache-manifest.cd8e6d30ef5c060b8c5110b591721204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad64ba542fd36a92242af3f6ef43bf08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b87bcdd728eb0dadf1e9",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "30e8fb958a0650185153",
+    "url": "/static/js/2.288ea622.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.288ea622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87bcdd728eb0dadf1e9",
+    "url": "/static/js/main.51939be1.chunk.js"
+  },
+  {
+    "revision": "f5472533625179562007",
+    "url": "/static/js/runtime-main.7f7f28fd.js"
+  }
+]);
